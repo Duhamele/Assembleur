7bb6e32736a4d4898fb11210439a69489218c2f5
duhamel <elie.duhamel@grenoble-inp.org>
2025-11-02 22:27:54 -0500

    asm pgcd tout OK

--- source/test-1/cal.asm
+++ source/test-1/cal.asm
@@ -1,9 +1,42 @@
 global somme
-
+global pgcd
 section .text
 
 
 somme:
     MOV rax,rdi
     add rax,rsi
+    ret
+
+
+
+pgcd:
+    cmp rdi,rsi
+    je pgcd.entre_egal
+.etape1:
+    cmp rdi,rsi
+    jg pgcd.etape1b
+    xor rdx, rdx; 
+    mov rax, rsi
+    div rdi
+    mov rsi,rdx
+    cmp rdx,0
+    jne pgcd.etape1
+    mov rax, rdi
+    ret
+
+
+.etape1b:
+    xor rdx, rdx; 
+    mov rax, rdi
+    div rsi 
+    mov rdi,rdx
+    cmp rdx,0
+    jne pgcd.etape1
+    mov rax, rsi
+    ret
+
+
+.entre_egal:
+    mov rax,rdi
     ret
---
+++ source/test-1/test-pgcd.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+extern int pgcd(int a,int b);
+
+int test_pgcd(int a,int b,int c){
+
+    printf("PGCD %i et %i\n",a,b);
+    int d=pgcd(a,b);
+    printf("\t%i\n",d);
+    if(c==d){
+        printf("Résultat Ok");
+        return 0;
+    }else{
+        printf("Résultat Err");
+        return -1;
+    }
+}
+
+int main(){
+    int test=0;
+    test=test||test_pgcd(12,10,2);
+    test=test||test_pgcd(12,15,3);
+    test=test||test_pgcd(12,12,12);
+    test=test||test_pgcd(8,10,2);
+
+    
+
+
+
+
+
+
+}
