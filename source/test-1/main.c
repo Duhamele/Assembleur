e6b9d4f7f9651f9fefa5848feb3816cd5945d98d
duhamel <elie.duhamel@grenoble-inp.org>
2025-11-04 11:52:30 -0500

    test somme

--- source/test-1/main.c
+++ source/test-1/main.c
@@ -1,14 +1,28 @@
 #include <stdio.h>
 extern int somme(int a,int b);
 
+int test_somme(int a,int b,int c){
+    printf("Somme entre %i et %i :\n",a,b);
+    int d =somme(a,b);
+    printf("\t%i\n",d);
+    if(c==d){
+        printf("Ok resultat\n");
+        return 0;
+    }else{
+        printf("Err\n");
+        return -1;
+    }
+}
 
 int main(){
-    int a=2,b=7;
-    int c=somme(a,b);
-    if (c==9){
-        printf("Ok resultat %i\n",c);
-    }else{
-        printf("Error");
-    }
+    int test=0;
+    test=test||test_somme(7,2,9);
+    test=test||test_somme(7,8,15);
+    test=test||test_somme(-1,2,1);
+    test=test||test_somme(7,3,10);
+    test=test||test_somme(7,0,7);
+    test=test||test_somme(-5,2,-3);
+
+    return test;
     
 }
